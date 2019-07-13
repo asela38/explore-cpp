9021cb5361fa6d35ff0f3a3a85f55efa2877e8e2
asela <asela38@gmail.com>
2019-07-13 19:21:51 +0800

    Sperate class to new file

--- clazzes/Rectangle.cpp
+++ clazzes/Rectangle.cpp
@@ -1,37 +1,9 @@
 #include <iostream>
+
+#include "Rectangle.hpp"
+
 using namespace std;
 
-class Rectangle {
-	public:
-		Rectangle() = default;
-
-		Rectangle(double width, double height) 
-			: width_{width}, height_{height}
-		{}
-
-		double Width() const { return width_; }
-		double Height() const { return height_;}
-
-		double Area() const {
-			return width_ * height_;
-		}
-
-		double Perimeter() const {
-			return 2.0 * (width_ + height_);
-		}
-
-		void Scale(double scaleFactor) {
-			width_ *= scaleFactor;
-			height_ *= scaleFactor;
-
-		}
-
-	private:
-		double width_{};
-		double height_{};
-
-
-};
 
 void PrintInfo(const Rectangle& r) {
 
