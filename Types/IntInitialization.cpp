dfdb28fefc0b88ff025eea85fd95b6fd159082b6
asela <asela38@gmail.com>
2019-07-13 13:23:31 +0800

    Test on int

---
+++ Types/IntInitialization.cpp
@@ -0,0 +1,21 @@
+
+
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main() {
+
+	int i1;
+	cout << i1 << '\n';
+	int i2 = 110;
+	cout << i2 << '\n';
+	int i3(22);
+	cout << i3 << '\n';
+	int i4{21};
+	cout << i4 << '\n';
+	auto i5{42};
+	cout << i5 << '\n';
+
+}
